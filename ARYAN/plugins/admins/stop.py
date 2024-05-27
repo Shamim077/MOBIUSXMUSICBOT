48975b0db7312f741ed176983dc12fba52a453ef
Shamim077 <168723582+Shamim077@users.noreply.github.com>
2024-05-27 14:33:31 +0530

    Update stop.py

--- ARYAN/plugins/admins/stop.py
+++ ARYAN/plugins/admins/stop.py
@@ -1,11 +1,11 @@
 from pyrogram import filters
 from pyrogram.types import Message
 
-from ARYAN import app
-from ARYAN.core.call import ARYAN
-from ARYAN.utils.database import set_loop
-from ARYAN.utils.decorators import AdminRightsCheck
-from ARYAN.utils.inline import close_markup
+from GOJO import app
+from GOJO.core.call import ARYAN
+from GOJO.utils.database import set_loop
+from GOJO.utils.decorators import AdminRightsCheck
+from GOJO.utils.inline import close_markup
 from config import BANNED_USERS
 
 
@@ -16,7 +16,7 @@ from config import BANNED_USERS
 async def stop_music(cli, message: Message, _, chat_id):
     if not len(message.command) == 1:
         return
-    await ARYAN.stop_stream(chat_id)
+    await GOJO.stop_stream(chat_id)
     await set_loop(chat_id, 0)
     await message.reply_text(
         _["admin_5"].format(message.from_user.mention), reply_markup=close_markup(_)
