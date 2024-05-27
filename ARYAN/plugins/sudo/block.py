2af98f51649ef7694afcad5ddd1d2a090df409d5
Shamim077 <168723582+Shamim077@users.noreply.github.com>
2024-05-27 13:01:47 +0530

    Update block.py

--- ARYAN/plugins/sudo/block.py
+++ ARYAN/plugins/sudo/block.py
@@ -1,11 +1,11 @@
 from pyrogram import filters
 from pyrogram.types import Message
 
-from ARYAN import app
-from ARYAN.misc import SUDOERS
-from ARYAN.utils.database import add_gban_user, remove_gban_user
-from ARYAN.utils.decorators.language import language
-from ARYAN.utils.extraction import extract_user
+from GOJO import app
+from GOJO.misc import SUDOERS
+from GOJO.utils.database import add_gban_user, remove_gban_user
+from GOJO.utils.decorators.language import language
+from GOJO.utils.extraction import extract_user
 from config import BANNED_USERS
 
 
